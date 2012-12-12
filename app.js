0ad0f23e2a150b04fcd204f68b3b7dc1153dfe4b
digiNORI <good@diginori.com>
2012-12-11 21:15:56 -0500

    webaudio 동작

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-var app = require("express").createServer(),
-    io = require("socket.io").listen(app);
+//var app = require("express").createServer();
+var express = require('express'); 
+var app = module.exports = express.createServer();
+
+var io = require("socket.io").listen(app);
 
 // 실제 heroku에서는 PORT를 사용. 현재 Cloud9ide에선 C9_PORT사용
-//var port = process.env.C9_PORT;
-//if (!port){
+var port = process.env.C9_PORT;
+if (!port){
     port = process.env.PORT;
-//}
+}
 
-app.listen(88888888888);
+//configure
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.listen(port);
 
 app.get("/", function(req, res){
     res.sendfile(__dirname + "/index.html");
